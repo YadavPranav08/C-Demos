0f9003887ce3d45e495b8a140017a3d79e45f634
YadavPranav08 <151705687+YadavPranav08@users.noreply.github.com>
2024-01-04 10:41:06 +0530

    Boxing Unboxing

---
+++ 16_BoxingUnBoxing/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_BoxingUnBoxing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+            Console.WriteLine($"i = {i}");
+            object o = i;
+            Console.WriteLine($"o = {o}");  // int=> object / value type => referance type == boxing
+
+            int j = (int) o;
+            Console.WriteLine($"j = {j}"); // object=> int / referance type => value type == unboxing
+
+            // j = i;
+
+            // object is a base type for all type of dot net
+
+            object o1 = 10;
+            o1 = "$";
+            o1 = "abc";
+            o1 = 3.14f;
+            o1 = true;
+            
+
+
+            Console.ReadLine();
+        }
+    }
+}
