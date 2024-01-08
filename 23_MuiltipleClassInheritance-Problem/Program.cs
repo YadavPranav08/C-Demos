1d0bed98a55736d07b79b22003b035112455e33e
YadavPranav08 <151705687+YadavPranav08@users.noreply.github.com>
2024-01-08 10:51:16 +0530

    Multiple Class Inheritance

---
+++ 23_MuiltipleClassInheritance-Problem/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23_MuiltipleClassInheritance_Problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            A a1 = new A(); a1.Print();
+            B b1 = new B(); b1.Print();
+            A a2 = new B(); a2.Print();
+            D d1 = new D(); d1.Print();
+            
+
+            Console.ReadLine();
+        }
+    }
+
+    public class A
+    {
+        public virtual void Print() 
+        {
+            Console.WriteLine("A");
+        }
+    }
+
+    public class B : A
+    {
+        public override void Print()
+        {
+            Console.WriteLine("B");
+        }
+    }
+
+    public class D : B    // D:B:C (muiltiple class inheritance [not possible]) This is not possible in C# (Dimond Problem) // Due to Confusion(Ambugity)
+    {
+        //public void Print()
+        //{
+        //    Console.WriteLine("D");
+        //}
+    }
+}
