77dfe514e0603732994a39744ebf1be540f49506
YadavPranav08 <151705687+YadavPranav08@users.noreply.github.com>
2024-01-10 16:18:23 +0530

    Operator Overloding - Scenario

---
+++ 26_MethodOverloding-Scenario/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26_MethodOverloding_Scenario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            A a1 = new A();
+            a1.PrintA();
+
+            B b1 = new B();
+            b1.PrintA();     // coming from class A
+            b1.PrintA(10);  // Coming from class B
+
+            Console.ReadLine();
+        }
+    }
+
+    class A
+    {
+        public void PrintA()
+        {
+          Console.WriteLine("PrintA() Method Called" );
+        }
+        
+    }
+
+    class B : A
+    {
+        public void PrintA(int num)
+        {
+            Console.WriteLine("PrintA(int num) Method Called");
+        }
+
+    }
+}
