0171f16bdfa7e4360142f305ec9988f6e2d7e524
YadavPranav08 <151705687+YadavPranav08@users.noreply.github.com>
2024-01-17 19:13:03 +0530

    Exception Handling Scenario

---
+++ 39_ExceptionHandling-Scenario/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _39_ExceptionHandling_Scenario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = GetDeta();
+            Console.WriteLine(result);
+
+            string results = GetDeta1();
+            Console.WriteLine(results);
+
+
+            Console.ReadLine();
+        }
+
+        static string GetDeta()
+        {
+            try
+            {
+                
+                return "TRY";
+                
+            }
+            catch
+            {
+                
+                return "catch";
+                
+            }
+            finally
+            {
+                // return "finally";   // return statement is not possible in finally
+            }
+        }
+        static string GetDeta1()
+        {
+            string result = "";
+            try
+            {
+                result = "try";
+                //return "TRY";
+                return result;
+            }
+            catch
+            {
+                result = "catch";
+                //return "catch";
+                return result;
+            }
+            finally
+            {
+                Console.WriteLine("Finally Block Executed");
+                 result = "Finally";   // return statement is not possible in finally
+            }
+
+            return result;
+        }
+
+    }
+}
