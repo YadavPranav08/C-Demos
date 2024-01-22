2e87cdc7a0dd4a5c07a7205d9bae9e72447fa6d2
YadavPranav08 <151705687+YadavPranav08@users.noreply.github.com>
2024-01-22 13:41:25 +0530

    Enum

---
+++ 42_Enum/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _42_Enum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student();
+            s.Rollnumber = 1;
+            s.Name = "Pranav";
+            s.Gender = Gender.Male;
+            s.Subject = Subject.Sanscrit;
+
+            //Console.WriteLine($"Rollnumber: {s.Rollnumber}, Name: {s.Name}, Gender: {GetGender(s.Gender)}");
+            Console.WriteLine($"Rollnumber: {s.Rollnumber}, Name: {s.Name}, " +
+                $"Gender: {s.Gender}, Subject: {s.Subject}");
+
+
+            Student s1 = new Student();
+            s1.Rollnumber = 11;
+            s1.Name = "Ashu";
+            s1.Gender = Gender.Male;
+            s1.Subject = Subject.English;
+
+            Console.WriteLine($"Rollnumber: {s1.Rollnumber}, Name: {s1.Name}, Gender: {s1.Gender}");
+
+            Student s2 = new Student();
+            s2.Rollnumber = 09;
+            s2.Name = "Seeta";
+            s2.Gender = Gender.Male;
+            s2.Subject = Subject.Marathi;
+
+            Console.WriteLine($"Rollnumber: {s2.Rollnumber}, Name: {s2.Name}, Gender: {s2.Gender}");
+
+            //   //
+            Console.WriteLine("All Item From Enum");
+
+            string[] names= Enum.GetNames(typeof(Gender));
+
+            for(int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+            Console.WriteLine();    // just for new line
+
+            Console.WriteLine("All Value From Enum List");
+
+            int[] values = (int[]) Enum.GetValues(typeof(Gender));  
+
+            for(int i = 0; i < values.Length; i++)
+            {
+                Console.WriteLine(values[i]);
+            }
+            Console.WriteLine();    // just for new line
+
+
+            Console.ReadLine();
+        }
+        static string GetGender(int Gender)
+        {
+            switch (Gender)
+            {
+                case 1:
+                    return "Male";
+                case 2:
+                    return "Female";
+                default:
+                    return "INVALID GENDER";
+            }
+        }
+    }
+}
---
+++ 42_Enum/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _42_Enum
+{
+    public class Student
+    {
+        public int Rollnumber { get; set; }
+        public string Name { get; set; }
+        //public string Gender { get; set; }
+        //public int Gender { get; set; }
+        public Gender Gender { get; set; }
+
+        //public string Subject { get; set; }
+        public Subject Subject { get; set; }
+    }
+
+    public enum Gender
+    {
+        Male=1, Female, unknown
+    }
+    
+    public enum Subject
+    {
+     Marathi=1,
+     Sanscrit,
+     English,
+     Mathematics,
+     Science,
+     Social_Science,
+    }
+}
