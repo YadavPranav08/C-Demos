0cb0a5062b7b52b93aaa60765c3aa5334074963b
YadavPranav08 <151705687+YadavPranav08@users.noreply.github.com>
2024-01-25 11:35:53 +0530

    Multicast Delegates

---
+++ 44_MulticastDelegates/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _44_MulticastDelegates
+{
+    delegate string Printdelegate();
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Printdelegate pd = PrintA;
+            pd += PrintB;
+            pd += PrintC;
+
+            //string result = pd();
+            //Console.WriteLine(result);
+
+            Delegate[] delegates = pd.GetInvocationList();
+            
+            for (int i = 0; i < delegates.Length; i++)
+            {
+                try 
+                {
+                    var result = delegates[i].DynamicInvoke();
+                    Console.WriteLine(result);
+                }
+                catch
+                {
+                    Console.WriteLine("Exception Occured");
+                }
+                
+            }
+
+            Console.ReadLine();
+        }
+        static string PrintA()
+        {
+            return "PrintA";
+        }
+        static string PrintB()
+        {
+            throw new Exception("INTENTIONALLY EXCEPTION THROWN");
+            return "PrintB";
+        }
+        static string PrintC()
+        {
+            return "PrintC";
+        }
+    }
+}
