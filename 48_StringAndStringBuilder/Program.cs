a3380596c23c2f3271169cc3b9e73cc48e3269a9
YadavPranav08 <151705687+YadavPranav08@users.noreply.github.com>
2024-01-31 23:25:52 +0530

    String And String Builder

---
+++ 48_StringAndStringBuilder/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _48_StringAndStringBuilder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string intro = "My ";
+            //intro += "Name ";
+            //intro += "is ";
+            //intro += "Khan.";
+            //Console.WriteLine(intro);
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Hii ");
+            sb.Append("Good ");
+            sb.Append("Morning All Of You..!");
+            Console.WriteLine(sb.ToString());
+
+
+            Console.ReadLine();
+        }
+    }
+}
