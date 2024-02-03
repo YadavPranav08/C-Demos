29841e8f3f8de82d967a565b203d0276a6f5dd58
YadavPranav08 <151705687+YadavPranav08@users.noreply.github.com>
2024-02-03 11:41:31 +0530

    Reflection

---
+++ 50_Reflection/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _50_Reflection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // early binding
+            //Customer c1 = new Customer();
+            //c1.Print();
+
+            // Late binding / Reflection
+
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            Type customer = assembly.GetType("_50_Reflection.Customer");
+
+            object c = Activator.CreateInstance(customer);
+
+            MethodInfo printA = customer.GetMethod("PrintA");
+
+            printA.Invoke(c, null);
+
+            MethodInfo printB = customer.GetMethod("PrintB");
+            printB.Invoke(null, null);
+
+            Console.ReadLine();
+        }
+    }
+    public class Customer
+    {
+        public void PrintA()
+        {
+            Console.WriteLine("PrintA() Method Called");
+        }
+
+        public static void PrintB()
+        {
+            Console.WriteLine("PrintB() Method Called");
+        }
+    }
+
+}
